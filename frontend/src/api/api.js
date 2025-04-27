e47ca629c251420fb72fe268fd7b66df08a616b3
Ashish Bhusal <ashish.bhusal.16999@gmail.com>
2025-04-27 07:16:25 -0700

    deployment: Disable nginx config for now
    Run frontend and backend seperately

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || '/api';
+const baseURL = import.meta.env.VITE_API_URL || 'http://54.89.21.90:8000';
 
 const api = axios.create({
   baseURL,
